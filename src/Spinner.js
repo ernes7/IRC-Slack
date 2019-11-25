dd9f335b1ca23f241f2b4ab1a961ffb590bed926
Ernesto <ernechelsea@pdx.edu>
2019-11-25 13:40:52 -0800

    added side Panel and sign out button

--- src/Spinner.js
+++ src/Spinner.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Loader, Dimmer } from "semantic-ui-react";
 
 const Spinner = () => (
-  <Dimmer >
+  <Dimmer>
     <Loader size="huge" content={"Preparing Chat..."} />
   </Dimmer>
 );
