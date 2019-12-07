eda45a2b816e023a3b256e399bf28041b16eeaa8
Ernesto <ernechelsea@pdx.edu>
2019-12-06 21:54:33 -0800

    small touch

--- src/Spinner.js
+++ src/Spinner.js
@@ -3,7 +3,7 @@ import { Loader, Dimmer } from "semantic-ui-react";
 
 const Spinner = () => (
   <Dimmer>
-    <Loader size="huge" content={"Preparing Chat..."} />
+    <Loader size="huge" content={"Preparing IRC..."} />
   </Dimmer>
 );
 
